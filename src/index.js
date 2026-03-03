7a9dee553ded89d265d12033f2d81ed2cf835abc
SUBHRO71 <subhro7021@gmail.com>
2026-03-03 23:59:38 +0530

    utilities code

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ dotenv.config({
     path: "./.env"
 })
 
-connectDB();
+connectDB().then(() => {
+    app.on("error", (error) => {
+        console.log("Server error: ", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    })
+})
+.catch((error) => {
+    console.log("Error connecting to DB or starting server: ", error);
+});
